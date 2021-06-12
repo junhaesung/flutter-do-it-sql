e3abbfa32d21fa76c9bab7f889b69f80bea60190
Haeseong Jeon <seong0428@gmail.com>
2021-06-12 14:53:14 +0900

    SQLite 사용하기 - 데이터베이스 구조 추가

---
+++ lib/todo.dart
@@ -0,0 +1,21 @@
+class Todo {
+  String title;
+  String content;
+  bool active;
+  int id;
+
+  Todo(
+      {required this.title,
+      required this.content,
+      required this.active,
+      required this.id});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'active': active,
+    };
+  }
+}
